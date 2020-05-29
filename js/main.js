60528d9191ece9f61b678ef748e4b0db96acbeab
Oleg <justbloods28@gmail.com>
2020-05-30 00:05:09 +0300

    second

--- js/main.js
+++ js/main.js
@@ -9,3 +9,104 @@
 // Реализовать цепочку протитопов с помощью Object.create.
 // Реализовать цепочку протитопов с помощью непосредственного изменения прототипа (__proto__, setPrototypeOf)
 // Реализовать цепочку протитопов с помощью функций конструкторов.
+
+
+
+const objA = {
+    name: "Oleg",
+    showName() {
+        return this.name
+    }
+}
+
+const objB = {
+    surname: "Kolesnuk",
+    showFullName () {
+        return `My full name: ${this.name} ${this.surname}`
+    },
+    // __proto__: objA
+}
+
+const objC = {
+    age: 24,
+    showFullInfo () {
+        return `My name ${this.name} ${this.surname}
+I am ${this.age} years old`
+    },
+    // __proto__: objB
+}
+
+// Object.setPrototypeOf(objC, objB);
+// Object.setPrototypeOf(objB, objA);
+
+console.log('----------------------------------')
+console.log(objC, "через __proto__ или Object.setPrototypeOf()")
+console.log(objA.showName());
+console.log(objB.showFullName());
+console.log(objC.showFullInfo());
+
+
+const objCreateA = {
+    name: "Oleg",
+    showName() {
+        return this.name
+    }
+}
+
+const objCreateB = Object.create(objCreateA);
+objCreateB.surname = "Kolesnuk"
+objCreateB.showFullName = function () {
+    return `My full name: ${this.name} ${this.surname}`
+};
+
+const objCreateC = Object.create(objCreateB);
+objCreateC.age = 24;
+objCreateC.showFullInfo = function () {
+    return `My name ${this.name} ${this.surname}
+I am ${this.age} years old`
+};
+
+console.log('----------------------------------')
+console.log(objCreateB, "через Object.create")
+console.log(objCreateA.showName());
+console.log(objCreateC.showFullName());
+console.log(objCreateC.showFullInfo());
+
+
+function ObjA (name) {
+    this.name = name,
+    this.showName = function () {
+        return this.name
+    }
+}
+
+const objConstructorA = new ObjA("Oleg")
+
+
+function ObjB (surname) {
+    this.surname = surname,
+    this.showFullName = function (){
+        return `My full name: ${this.name} ${this.surname}`
+    }
+}
+
+ObjB.prototype = objConstructorA;
+const objConstructorB = new ObjB("Kolesnuk");
+
+
+function ObjC (age) {
+    this.age = age,
+    this.showFullInfo = function () {
+        return `My name ${this.name} ${this.surname}
+I am ${this.age} years old`
+    }
+}
+
+ObjC.prototype = objConstructorB;
+const objConstructorC = new ObjC(24);
+
+console.log('----------------------------------')
+console.log(objConstructorC, 'Через конструктор')
+console.log(objConstructorC.showFullName())
+console.log(objConstructorC.showName())
+console.log(objConstructorC.showFullInfo())
